945a43cb6a5d4bc160133fb9be1bc2403966aacb
ChenJinchuang <qinchenju@gmail.com>
2019-04-30 16:45:17 +0800

    修复了权限判断BUG

--- application/lib/auth/Auth.php
+++ application/lib/auth/Auth.php
@@ -51,7 +51,7 @@ class Auth
         }
 
         // 判断接口权限是否在账户拥有权限数组内
-        if (in_array($actionAuth, $authList)) {
+        if (in_array(key($actionAuth), $authList)) {
             return true;
         } else {
             return false;
