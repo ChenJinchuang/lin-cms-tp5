2b699d07805fb0d0d787bc638a7116f588e99a59
aliqiqi <48013520+aliqiqi@users.noreply.github.com>
2019-06-03 20:28:54 +0800

    优化配置环境错误提示

--- application/lib/exception/token/DeployException.php
+++ application/lib/exception/token/DeployException.php
@@ -11,6 +11,6 @@ use LinCmsTp5\exception\BaseException;
 class DeployException extends BaseException
 {
     public $code = 500;
-    public $msg  = '请修改服务器环境配置为：opcache.save_comments=1';
+    public $msg  = '请修改php.ini配置：opcache.save_comments=1或直接注释掉此配置(无效请在 etc/php.d/ext-opcache.ini 文件中修改)';
     public $error_code = 50000;
 }
