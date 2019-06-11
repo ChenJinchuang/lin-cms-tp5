cd2e656c893becbabee7fa5841d60383b6ebcbd3
rooterWzs <17732270503@163.com>
2019-06-11 11:12:34 +0800

    new commit

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => 'localhost',
     // 数据库名
-    'database'        => 'lin-test',
+    'database'        => 'php-lin-cms',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
