2403f7a91e9f148e237e81435d9f9a6961594654
Chenjinchuang <qinchenju@gmail.com>
2020-09-05 19:12:40 +0800

    feat:优化了令牌的实现，集成二次封装的令牌工具库

--- application/api/controller/cms/User.php
+++ application/api/controller/cms/User.php
@@ -4,42 +4,67 @@ namespace app\api\controller\cms;
 
 //use app\api\validate\user\LoginForm;  # 开启注释验证器以后，本行可以去掉，这里做更替说明
 //use app\api\validate\user\RegisterForm; # 开启注释验证器以后，本行可以去掉，这里做更替说明
-use app\lib\token\Token;
+use app\api\service\token\LoginToken;
+use app\lib\exception\token\TokenException;
+use LinCmsTp5\admin\exception\user\UserException;
 use LinCmsTp5\admin\model\LinUser;
-use think\Controller;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
+use think\Exception;
+use think\exception\DbException;
 use think\facade\Hook;
+use think\Model;
 use think\Request;
+use think\response\Json;
 
-class User extends Controller
+class User
 {
+
+    /**
+     * @var LoginToken
+     */
+    private $loginTokenService;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->loginTokenService = LoginToken::getInstance();
+    }
+
     /**
      * 账户登陆
      * @param Request $request
      * @validate('LoginForm')
      * @return array
-     * @throws \think\Exception
+     * @throws Exception
      */
     public function login(Request $request)
     {
-//        (new LoginForm())->goCheck();  # 开启注释验证器以后，本行可以去掉，这里做更替说明
+        //        (new LoginForm())->goCheck();  # 开启注释验证器以后，本行可以去掉，这里做更替说明
         $params = $request->post();
 
         $user = LinUser::verify($params['username'], $params['password']);
-        $result = Token::getToken($user);
-
+        $token = $this->loginTokenService->getToken($user->toArray());
         Hook::listen('logger', array('uid' => $user->id, 'username' => $user->username, 'msg' => '登陆成功获取了令牌'));
 
-        return $result;
+        return [
+            'access_token' => $token['accessToken'],
+            'refresh_token' => $token['refreshToken']
+        ];
     }
 
     /**
      * 用户更新信息
      * @param Request $request
+     * @return Json
+     * @throws UserException
      */
     public function update(Request $request)
     {
         $params = $request->put();
-        $uid = Token::getCurrentUID();
+        $uid = $this->loginTokenService->getCurrentUid();
         LinUser::updateUserInfo($uid, $params);
         return writeJson(201, '', '操作成功');
     }
@@ -48,15 +73,13 @@ class User extends Controller
      * 修改密码
      * @validate('ChangePasswordForm')
      * @param Request $request
-     * @return \think\response\Json
-     * @throws \LinCmsTp5\admin\exception\user\UserException
-     * @throws \app\lib\exception\token\TokenException
-     * @throws \think\Exception
+     * @return Json
+     * @throws UserException
      */
     public function changePassword(Request $request)
     {
         $params = $request->put();
-        $uid = Token::getCurrentUID();
+        $uid = $this->loginTokenService->getCurrentUid();
         LinUser::changePassword($uid, $params);
 
         Hook::listen('logger', '修改了自己的密码');
@@ -66,12 +89,15 @@ class User extends Controller
 
     /**
      * 查询自己拥有的权限
-     * @throws \app\lib\exception\token\TokenException
-     * @throws \think\Exception
+     * @return array|string|Model
+     * @throws UserException
+     * @throws DataNotFoundException
+     * @throws ModelNotFoundException
+     * @throws DbException
      */
     public function getAllowedApis()
     {
-        $uid = Token::getCurrentUID();
+        $uid = $this->loginTokenService->getCurrentUid();
         $result = LinUser::getUserByUID($uid);
         return $result;
     }
@@ -80,16 +106,15 @@ class User extends Controller
      * @auth('创建用户','管理员','hidden')
      * @param Request $request
      * @validate('RegisterForm')
-     * @return \think\response\Json
-     * @throws \think\Exception
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\exception\DbException
+     * @return Json
+     * @throws Exception
+     * @throws DataNotFoundException
+     * @throws ModelNotFoundException
+     * @throws DbException
      */
     public function register(Request $request)
     {
-//        (new RegisterForm())->goCheck(); # 开启注释验证器以后，本行可以去掉，这里做更替说明
-
+        // (new RegisterForm())->goCheck(); # 开启注释验证器以后，本行可以去掉，这里做更替说明
         $params = $request->post();
         LinUser::createUser($params);
 
@@ -100,26 +125,24 @@ class User extends Controller
 
     /**
      * @return mixed
-     * @throws \app\lib\exception\token\TokenException
-     * @throws \think\Exception
      */
     public function getInformation()
     {
-        $user = Token::getCurrentUser();
-        return $user;
+        $uid = $this->loginTokenService->getCurrentUid();
+        $user = LinUser::get($uid);
+        return $user->hidden(['password']);
     }
 
     /**
      * @param Request $request
-     * @param ('url','头像url','require|url')
-     * @return \think\response\Json
-     * @throws \app\lib\exception\token\TokenException
-     * @throws \think\Exception
+     * @return Json
+     * @throws UserException
      */
     public function setAvatar(Request $request)
     {
         $url = $request->put('avatar');
-        $uid = Token::getCurrentUID();
+        $uid = $this->loginTokenService->getCurrentUid();
+
         LinUser::updateUserAvatar($uid, $url);
 
         return writeJson(201, '', '更新头像成功');
@@ -128,13 +151,13 @@ class User extends Controller
 
     /**
      * @return array
-     * @throws \app\lib\exception\token\TokenException
-     * @throws \think\Exception
+     * @throws TokenException
+     * @throws Exception
      */
     public function refresh()
     {
-        $result = Token::refreshToken();
+        $token = $this->loginTokenService->getTokenFromHeaders();
+        $result = $this->loginTokenService->refresh($token);
         return $result;
     }
-
 }
--- config/database.php
+++ config/database.php
@@ -13,9 +13,9 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'lin-test',
+    'database'        => 'lin-cms',
     // 用户名
     'username'        => 'root',
     // 密码
